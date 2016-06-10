3c7103f8c996a59dbea856b4b37cdd17bc9e4a86
Rony Nasr <nasr.rony@gmail.com>
2016-06-09 23:15:06 -0700

    added some logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,33 @@
-/*This file is for your custom js.  All yours*/
+//back end logic
+var pingPong = function (number) {
+  var result ="";
+  for (var i=1;i<=number;i++){
+    console.log(i);
+    if (i%3 !== 0 && i%5 !== 0){
+      result = result + "<li>" + i + "</li>";
+    }else {
+      result = result + "<li>"
+      if(i%3 === 0){
+        result = result + "ping";
+      }
+      if(i%5 === 0){
+        result = result + "pong"
+      }
+      result = result + "</li>"
+    }
+  }
+  console.log(result);
+  return result;
+}
+
+// Front end logic
+$(function () {
+  $("form#input").submit(function(event){
+    event.preventDefault();
+    var number = parseInt($("#number").val());
+    console.log(number);
+
+    $("ul#result").append(pingPong(number));
+
+  });
+});
